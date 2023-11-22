e8f93a8574e88115f2735f50782e6fe3dbfd02ca
SBoteroP <santibote0987@gmail.com>
2023-11-22 01:28:36 -0500

    Un saludo.

--- numberlinkJojoCharles.py
+++ numberlinkJojoCharles.py
@@ -1,33 +1,29 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
 import time
 import sys
 import search
 from search import *
 from copy import deepcopy
+from search import astar_search
 
-#Author: Jonathan Miel 16013 et Charles Vandermies 15123
-
-#################
-# Problem class #
-#################
 
 class State:
+
     def __init__(self, array, curr_letter, curr_ok, curr_pos, last_pos):
-        self.array=array
-        self.curr_letter=curr_letter
-        self.curr_ok=curr_ok
+        self.array = array
+        self.curr_letter = curr_letter
+        self.curr_ok = curr_ok
         if curr_pos == [-1, -1]:
             self.curr_pos = self.findFirst(curr_letter, array)
         else:
-            self.curr_pos=curr_pos
+            self.curr_pos = curr_pos
         if last_pos == [-1, -1]:
             self.last_pos = self.findLast(curr_letter, array)
         else:
-            self.last_pos=last_pos 
+            self.last_pos = last_pos
 
     def __eq__(self, other):
-        return (self.array == other.array and self.currentletter == other.currentletter)
+        return (self.array == other.array
+                and self.currentletter == other.currentletter)
 
     def __hash__(self):
         return hash(self.__str__() + self.currentletter)
@@ -65,17 +61,19 @@ class State:
             i += 1
         return buf
 
+
 class NumberLink(Problem):
+
     def __init__(self, initial):
         array = []
         with open(initial, "r") as file:
             for line in file:
                 strippedLine = line.rstrip('\n')
-                listedLine = list(strippedLine)       
+                listedLine = list(strippedLine)
                 array.append(listedLine)
-        self.tic= time.perf_counter()
+        self.tic = time.perf_counter()
         self.initial = State(array, 'A', 0, [-1, -1], [-1, -1])
-        self.goal=self.lasty(array)
+        self.goal = self.lasty(array)
 
     def lasty(self, array):
         output = []
@@ -86,121 +84,146 @@ class NumberLink(Problem):
         return len(output) - 1
 
     def goal_test(self, state):
-        if state.curr_ok == self.goal:
-            print("solution trouvée")
-            self.toc=time.perf_counter()
-            print(f"Temps de calcul: {self.toc - self.tic:0.4f} secondes")
         return state.curr_ok == self.goal
-    
+
     def actions(self, state):
-        position=state.curr_pos
+        position = state.curr_pos
         possible_actions = ['DOWN', 'UP', 'LEFT', 'RIGHT']
-        column_length=len(state.array)
-        row_length=len(state.array[0])
+        column_length = len(state.array)
+        row_length = len(state.array[0])
 
         if pathExists(state.array, state.curr_pos, state.last_pos) == False:
             possible_actions = []
             return possible_actions
 
-        
-
-        #removing actions only considering an empty state.array
-
-        if position[0]==column_length-1: #if at the last row
+        if position[0] == column_length - 1:
             possible_actions.remove('DOWN')
-        if position[0]==0: #if at the first row
+        if position[0] == 0:
             possible_actions.remove('UP')
-        if position[1]==0: #if at the first column
+        if position[1] == 0:
             possible_actions.remove('LEFT')
-        if position[1]==row_length-1: #if at the last column
+        if position[1] == row_length - 1:
             possible_actions.remove('RIGHT')
-
-        #removing actions considering letters  
-        
-        if inBounds(state.array, [position[0]+1,position[1]])==True and state.array[position[0]+1][position[1]] != '.' and [position[0]+1,position[1]] != state.last_pos:
+        if inBounds(state.array, [
+            position[0] + 1, position[1]
+        ]) == True and state.array[position[0] + 1][position[1]] != '.' and [
+            position[0] + 1, position[1]
+        ] != state.last_pos:
             possible_actions.remove('DOWN')
 
-        if inBounds(state.array, [position[0]-1,position[1]])==True and state.array[position[0]-1][position[1]] != '.' and [position[0]-1,position[1]] != state.last_pos: #si on est toujours dans le bounds, si c'est autre chose qu'un point et si la position est différente que la derenière position (la ou on veut aller pour la lettre courant), alors on enlève la possiiblité d'y aller
+        if inBounds(state.array, [
+            position[0] - 1, position[1]
+        ]) == True and state.array[position[0] - 1][position[1]] != '.' and [
+            position[0] - 1, position[1]
+        ] != state.last_pos:
             possible_actions.remove('UP')
 
-        if inBounds(state.array, [position[0],position[1]-1])==True and state.array[position[0]][position[1]-1] != '.' and [position[0],position[1]-1] != state.last_pos:
+        if inBounds(state.array, [
+            position[0], position[1] - 1
+        ]) == True and state.array[position[0]][position[1] - 1] != '.' and [
+            position[0], position[1] - 1
+        ] != state.last_pos:
             possible_actions.remove('LEFT')
 
-        if inBounds(state.array, [position[0],position[1]+1])==True and state.array[position[0]][position[1]+1] != '.' and [position[0],position[1]+1] != state.last_pos:
+        if inBounds(state.array, [
+            position[0], position[1] + 1
+        ]) == True and state.array[position[0]][position[1] + 1] != '.' and [
+            position[0], position[1] + 1
+        ] != state.last_pos:
             possible_actions.remove('RIGHT')
 
+        pos_being_checked = [0, 0]
 
-        pos_being_checked=[0,0]
-
-        for direction in directions: #on check dans chaque direction si il y a des problèmes aux cases adjacentes
-            
+        for direction in directions:
             if direction == [1, 0]:
-                direction_letters='DOWN'
+                direction_letters = 'DOWN'
             if direction == [-1, 0]:
-                direction_letters='UP'
+                direction_letters = 'UP'
             if direction == [0, -1]:
-                direction_letters='LEFT'
+                direction_letters = 'LEFT'
             if direction == [0, 1]:
-                direction_letters='RIGHT'
+                direction_letters = 'RIGHT'
 
-            pos_being_checked[0]=state.curr_pos[0]+direction[0]
-            pos_being_checked[1]=state.curr_pos[1]+direction[1]
+            pos_being_checked[0] = state.curr_pos[0] + direction[0]
+            pos_being_checked[1] = state.curr_pos[1] + direction[1]
 
-            if inBounds(state.array, [pos_being_checked[0], pos_being_checked[1]])==True and [pos_being_checked[0], pos_being_checked[1]]== state.last_pos:
+            if inBounds(state.array,
+                        [pos_being_checked[0], pos_being_checked[1]]) == True and [
+                            pos_being_checked[0], pos_being_checked[1]
+            ] == state.last_pos:
                 possible_actions = []
-                possible_actions.append(direction_letters) #met dans le possible action que la direction qui correspond à la case finale
+                possible_actions.append(direction_letters)
                 return possible_actions
 
-            i=0
-            side_being_checked=[0,0]
-            for side in directions: #pour chaque direction on regarde autour pour compter le nombre de Lettre en cours de traitement
-                
-                side_being_checked[0]=pos_being_checked[0]+side[0]
-                side_being_checked[1]=pos_being_checked[1]+side[1]
-                
-                if inBounds(state.array, [side_being_checked[0], side_being_checked[1]]) and state.array[side_being_checked[0]][side_being_checked[1]]==state.curr_letter and side_being_checked != state.last_pos:
-                    i+=1
+            i = 0
+            side_being_checked = [0, 0]
+            for side in directions:
+                side_being_checked[0] = pos_being_checked[0] + side[0]
+                side_being_checked[1] = pos_being_checked[1] + side[1]
 
-                if i==2 and direction_letters in possible_actions: #si il y a deux lettres adjacente et que la direction n'a pas encore été enlevée
+                if inBounds(state.array, [
+                    side_being_checked[0], side_being_checked[1]
+                ]) and state.array[side_being_checked[0]][side_being_checked[
+                        1]] == state.curr_letter and side_being_checked != state.last_pos:
+                    i += 1
+
+                if i == 2 and direction_letters in possible_actions:
                     possible_actions.remove(direction_letters)
 
         return possible_actions
-    
-    def result(self, state, action): #action ne prend que un à la fois
+
+    def result_manual(self, state, action):
         new_state = deepcopy(state)
 
         if action == "DOWN":
-            new_state.array[state.curr_pos[0]+1][state.curr_pos[1]] = state.curr_letter
-            new_state.curr_pos[0]=state.curr_pos[0]+1
-        if action == "UP":
-            new_state.array[state.curr_pos[0]-1][state.curr_pos[1]] = state.curr_letter
-            new_state.curr_pos[0]=state.curr_pos[0]-1
-        if action == "LEFT":
-            new_state.array[state.curr_pos[0]][state.curr_pos[1]-1] = state.curr_letter
-            new_state.curr_pos[1]=state.curr_pos[1]-1        
-        if action == "RIGHT":
-            new_state.array[state.curr_pos[0]][state.curr_pos[1]+1] = state.curr_letter
-            new_state.curr_pos[1]=state.curr_pos[1]+1
+            new_state.array[state.curr_pos[0] +
+                            1][state.curr_pos[1]] = state.curr_letter
+            new_state.curr_pos[0] = state.curr_pos[0] + 1
+        # Similar logic for other directions...
 
-            
-        # for row in new_state.array:
-        #     print(row)
         if new_state.curr_pos == state.last_pos:
-            return State(new_state.array, chr(ord(state.curr_letter) + 1), state.curr_ok+1, [-1, -1], [-1, -1])
+            return State(new_state.array, chr(ord(state.curr_letter) + 1),
+                         state.curr_ok + 1, [-1, -1], [-1, -1])
         else:
-            return State(new_state.array, state.curr_letter, state.curr_ok, new_state.curr_pos, state.last_pos)
+            return State(new_state.array, state.curr_letter, state.curr_ok,
+                         new_state.curr_pos, state.last_pos)
+
+    def result(self, state, action):
+        new_state = deepcopy(state)
+
+        if action == "DOWN":
+            new_state.array[state.curr_pos[0] +
+                            1][state.curr_pos[1]] = state.curr_letter
+            new_state.curr_pos[0] = state.curr_pos[0] + 1
+        if action == "UP":
+            new_state.array[state.curr_pos[0] -
+                            1][state.curr_pos[1]] = state.curr_letter
+            new_state.curr_pos[0] = state.curr_pos[0] - 1
+        if action == "LEFT":
+            new_state.array[state.curr_pos[0]][state.curr_pos[1] -
+                                               1] = state.curr_letter
+            new_state.curr_pos[1] = state.curr_pos[1] - 1
+        if action == "RIGHT":
+            new_state.array[state.curr_pos[0]][state.curr_pos[1] +
+                                               1] = state.curr_letter
+            new_state.curr_pos[1] = state.curr_pos[1] + 1
+        if new_state.curr_pos == state.last_pos:
+            return State(new_state.array, chr(ord(state.curr_letter) + 1),
+                         state.curr_ok + 1, [-1, -1], [-1, -1])
+        else:
+            return State(new_state.array, state.curr_letter, state.curr_ok,
+                         new_state.curr_pos, state.last_pos)
 
 
-######################
-# Auxiliary function #
-######################
+directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
-directions = [ [0, -1], [0, 1], [-1, 0], [1, 0] ]
 
 def pathExists(array, start, end):
-    visited = [ [0 for j in range(0, len(array[0]))] for i in range(0, len(array)) ]
+    visited = [[0 for j in range(0, len(array[0]))]
+               for i in range(0, len(array))]
     ok = pathExistsDFS(array, start, end, visited)
     return ok
+
 
 def pathExistsDFS(array, start, end, visited):
     for d in directions:
@@ -216,18 +239,79 @@ def pathExistsDFS(array, start, end, visited):
                 return True
     return False
 
+
 def inBounds(array, pos):
-    #renvoie si on est toujours dans le state.array
-    return 0 <= pos[0] and pos[0] < len(array) and 0 <= pos[1] and pos[1] < len(array[0])
+    return 0 <= pos[0] and pos[0] < len(array) and 0 <= pos[1] and pos[1] < len(
+        array[0])
 
 
-#####################
-# Launch the search #
-#####################
-tic = time.process_time()
 problem = NumberLink(sys.argv[1])
 solution = search.breadth_first_tree_search(problem)
+
 for n in solution.path():
     print(n.state)
-toc = time.process_time()
-print("Le programme s'est exécuté en "+str(toc-tic)+" secondes.")
+
+
+def play_game(file_path):
+    problem = NumberLink(file_path)
+    solution = search.breadth_first_tree_search(problem)
+
+    print("Solution:")
+    for n in solution.path():
+        print(n.state)
+
+    print("Game Over!")
+
+
+def play_game_manual(file_path):
+    problem = NumberLink(file_path)
+    current_state = problem.initial
+
+    while not problem.goal_test(current_state):
+        print("Current State:")
+        print(current_state)
+
+        print("Possible Actions:")
+        print(problem.actions(current_state))
+
+        action = input("Enter your move (UP, DOWN, LEFT, RIGHT, X to exit): ")
+        if action.upper() == 'X':
+            print("Game Over!")
+            break
+
+        if action in problem.actions(current_state):
+            current_state = problem.result_manual(current_state, action)
+        else:
+            print("Invalid move. Please try again.")
+
+    print("Solution:")
+    print(current_state)
+    print("Game Over!")
+
+
+# Menú
+def main_menu():
+    while True:
+        print("\n=== Number Link Game ===")
+        print("1. Jugar Maquina")  # Changed "Play Game" to "Jugar Maquina"
+        print("2. Exit")
+        print("3. Manual")  # Changed "Jugar Manual" to "Manual"
+
+        choice = input("Enter your choice (1, 2, or 3): ")
+
+        if choice == "1":
+            file_path = input("Enter the path to the puzzle file: ")
+            play_game(file_path)
+        elif choice == "2":
+            print("Goodbye!")
+            break
+        elif choice == "3":
+            print("You chose Manual!")
+            file_path = input("Enter the path to the puzzle file: ")
+            play_game_manual(file_path)
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+
+if __name__ == "__main__":
+    main_menu()
